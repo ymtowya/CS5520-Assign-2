2263115da3852042f6fb3339cb36b7bd3f358ce2
ymtowya <kaigendo@126.com>
2023-02-25 13:31:29 -0500

    Screen Structure

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
+
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="All" component={}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
