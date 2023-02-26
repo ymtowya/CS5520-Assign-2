4ab9c58a334c07481c4a63ebc11c537f28560f3d
ymtowya <kaigendo@126.com>
2023-02-26 13:08:06 -0500

    struct of edit entry

--- screen/EditEntry.js
+++ screen/EditEntry.js
@@ -1,13 +1,37 @@
 import { useEffect } from "react";
 import { View, Text } from "react-native";
+import ButtonPressable from "../component/ButtonPressable";
 
-export default function EditEntry({ route, navigation }) {
+export default function EditEntry({ route, navigation, meal }) {
+
+    const { calory, mealName, id } = meal;
 
     return (
         <View>
-            <Text>
-                Edit Entry
-            </Text>
+            <View>
+                <View>
+                    <Text>
+                        Calories
+                    </Text>
+                    <Text>
+                        { calory }
+                    </Text>
+                </View>
+                <View>
+                    <Text>
+                        Description
+                    </Text>
+                    <Text>
+                        { mealName }
+                    </Text>
+                </View>
+                <ButtonPressable>
+                    del
+                </ButtonPressable>
+                <ButtonPressable>
+                    rev
+                </ButtonPressable>
+            </View>
         </View>
     );
 }
