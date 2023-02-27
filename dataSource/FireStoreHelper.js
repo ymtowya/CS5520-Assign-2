391e15d1554d93eccc757a0fb9975d929df6c5e6
ymtowya <kaigendo@126.com>
2023-02-26 23:39:15 -0500

    update doc

--- dataSource/FireStoreHelper.js
+++ dataSource/FireStoreHelper.js
@@ -1,4 +1,4 @@
-import { addDoc, collection, doc } from "firebase/firestore";
+import { addDoc, collection, doc, updateDoc } from "firebase/firestore";
 import { firestore } from "./firebase-setup";
 
 const collectId = 'meals';
@@ -8,7 +8,7 @@ export async function writeToDB( { data } ) {
         const docRef = await addDoc(collection(firestore, collectId), data);
         console.log("Document writted id: ", docRef.id);
     } catch (err) {
-       console.log(err); 
+       console.err(err); 
     }
 }
 
@@ -16,7 +16,18 @@ export async function deleteFromDB({ docid }) {
     try {
         await deleteDoc(doc(firestore, collectId, docid));
     } catch (err) {
-        console.log(err);
+        console.err(err);
+    }
+}
+
+export async function updateDB({ docid }) {
+    try {
+        const docRef = doc(firestore, collectId, docid);
+        await updateDoc(docRef, {
+            reviewed: true
+        });
+    } catch (err) {
+        console.err(err);
     }
 }
 
